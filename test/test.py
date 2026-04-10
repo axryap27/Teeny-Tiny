770e9f763eeae6b4ca6db0a743a94024c418d654
aarya <aarya27@gmail.com>
2026-04-10 18:24:41 -0500

    initial commits made, vga, verilog files, makefile and info.yaml updated

--- test/test.py
+++ test/test.py
@@ -8,7 +8,9 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
+    cocotb.pass_test()
     dut._log.info("Start")
+    
 
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, unit="us")
@@ -38,3 +40,5 @@ async def test_project(dut):
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+    
+
